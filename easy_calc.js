0e26b40506075aecd136b252fa30e188b5e066e5
Jessica Schofield <67497575+JessS1987@users.noreply.github.com>
2021-11-01 20:34:08 +0000

    Calculator homework
    New calculator

---
+++ easy_calc.js
@@ -0,0 +1,58 @@
+//simple calculator 
+
+//addition function
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+//Substract function
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+//Divide function
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+//Multiply function
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+//Power function
+function power(num1, num2) {
+    return num1 ** num2;
+}
+
+//Square function
+function squareRoot(num1, num2) {
+   return Math.sqrt(num1);
+   return Math.sqrt(num2);
+}
+
+
+let operator = prompt("Enter operator ( either +, -, /, x, power or square root): ");
+
+let num1 = prompt("enter a number");
+let num2 = prompt("enter a second number");
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let result;
+
+  if (operator == '+'){
+   alert(`${num1} + ${num2} = ${sum(num1, num2)}`)
+} else if (operator == "-"){
+    alert(`${num1} - ${num2} = ${subtract(num1, num2)}`)
+} else if (operator == "/"){
+    alert(`${num1} / ${num2} = ${divide(num1, num2)}`)
+} else if (operator == "x"){
+    alert(`${num1} * ${num2} = ${multiply(num1, num2)}`);
+} else if (operator == "power"){
+    alert(`The power of ${num1} and ${num2} is ${power(num1,num2)} `);
+} else if (operator == "square root"){
+   alert (`The square roots of ${num1} and ${num2} are ${Math.sqrt(num1)} and ${Math.sqrt(num2)}`);
+} else 
+  alert(`uh oh! somethings gone awry!`)
+   
